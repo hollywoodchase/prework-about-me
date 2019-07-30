664d6650c0364f98be0e4d616c9ef81c7a7a5636
hollywoodchase <32149505+hollywoodchase@users.noreply.github.com>
2019-07-30 10:23:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,11 @@ anime.timeline({loop: true})
     easing: "easeOutExpo",
     delay: 1000
   });
+
+  var coll = document.getElementsByClassName('collapsible');
+
+  for (let i = 0; i < coll.length; i++) {
+  	coll[i].addEventListener('click', function() {
+  		(this.nextElementSibling).classList.toggle('active');
+  	});
+  }
